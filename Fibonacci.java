1b670530807263c68b66bf2b49f3dc4905c6583e
ArunAshwin08 <ashwinarun37@gmail.com>
2024-08-23 23:20:07 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+package week1day2;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int input = 10;
+	int a = 0;
+	int b=1;
+	System.out.print("fibonacci series are "+ a +" " +b);
+	for (int i = 2; i <= input; i++) {
+		int c=a+b;
+	System.out.print(" "+ c);
+	a=b;
+	b=c;
+	}
+	}
+}
